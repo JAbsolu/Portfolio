86340f53c110301d11f3c876eda9cd03e1e51a71
JAbsolu <90818638+JAbsolu@users.noreply.github.com>
2022-05-30 01:26:12 -0400

    Update script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+const toggle_menu = document.querySelector(".hamburger_toggle");
+const hamburger = document.querySelectorAll(".hamburger__menu");
+const show_nav = document.querySelector(".navigation");
+
+toggle_menu.addEventListener('click', () => {
+    hamburger.forEach(bar => {
+        bar.classList.toggle('active');
+    })
+    show_nav.classList.toggle("active");
+})
